adb156e01beb79a91348bab5db96d6ed21ec3baf
allexis096 <allexissoares@gmail.com>
2020-11-06 11:10:21 -0300

    :zap: chore: added routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+
+      <GlobalStyles />
+    </>
   );
 };
 
