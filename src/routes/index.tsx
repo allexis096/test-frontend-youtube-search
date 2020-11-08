e086d7fbb9eda244adee1e9f918739f0026af893
allexis096 <allexissoares@gmail.com>
2020-11-08 13:34:31 -0300

    :zap: chore: routes using VideoProvider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Dashboard from '../pages/Dashboard';
+import Video from '../pages/Video';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/video/:video+" component={Video} />
   </Switch>
 );
 
